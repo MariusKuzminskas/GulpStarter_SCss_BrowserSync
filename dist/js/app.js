e706d5e3b239db584aeaa7e24cd2e92c678565d2
mariuskuzminskas <kuzimnskas.marius7@gmail.com>
2020-07-23 16:24:04 +0300

    validation works

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,22 +1,42 @@
 console.log('hello');
 
-
 const inputEl = document.getElementById('input');
 const btnEl = document.getElementById('btn');
 const formEl = document.querySelector('.form');
+const errorEl = document.querySelector('.form__msg--error');
 
-formEl.addEventListener('click', e => {
+formEl.addEventListener('submit', e => {
     e.preventDefault();
-    console.log(inputEl.value);
+    const inputValue = inputEl.value;
+    if (inputValue && inputValue != 0) {
+        // console.log("not null");
+        if (emailIsValid(inputValue)) {
+            // console.log("valid");
+            // const validEmail = inputValue;
+            sendPostRequest(inputValue);
+            errorEl.innerText = 'Thank you. Your email has been sent';
+            errorEl.classList.add('form__msg--success');
+            inputEl.value = '';
 
-    if (emailIsValid(inputEl.value)) {
-
+        } else {
+            errorEl.innerText = "please check your email";
+        }
+    } else {
+        errorEl.innerText = "please enter an email";
+        return;
     }
 
+})
 
+// functions
+
+function emailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
+
+function sendPostRequest(validEmail) {
     axios.post('https://reqres.in/api/users', {
-            name: 'Fred',
-            job: 'Flintstone'
+            email: validEmail
         }, {
             headers: {
                 'X-ShopId': 'sh_9f57832f-456b-44f3-888f-8a370b155a18',
@@ -29,9 +49,4 @@ formEl.addEventListener('click', e => {
         .catch(function(error) {
             console.log(error);
         });
-})
-
-
-function emailIsValid(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
