a70f3de2fb487c5aa3faa3b0bda35e035abc6ac2
mariuskuzminskas <kuzimnskas.marius7@gmail.com>
2020-07-23 15:29:40 +0300

    getting the value from input

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+const inputEl = document.getElementById('input');
+const btnEl = document.getElementById('btn');
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(inputEl.value);
+})
